74952eb050b68d23295c2a9b6fb22c1998dc03e6
kowsik95 <kowsik.mvi@gmail.com>
2022-02-25 16:48:03 +0530

    update

---
+++ d6t2.js
@@ -0,0 +1,145 @@
+// <!DOCTYPE html>
+// <html>
+// <body>
+// Whats the error in this ?
+
+// alert( “I'm JavaScript!");
+//  <script>
+//  </script>
+// </body>
+// </html>
+
+// <!DOCTYPE html>
+// <html>
+// <body>
+//  <script src="script.js"></script>
+// </body>
+// </html> 
+
+// <!DOCTYPE html>
+// <html>
+// <body>
+//  <script src=”script.js”></script>
+// The source file with the required functions on the Javascript
+// will get executed when the required actions are made in the
+// </body>
+// </html>
+
+// let admin=9, fname=10.5; 
+// Here the value of admin and fname is assigned with the numbers,
+// but the requirement for the alert is Guvi Geek that is assigned sepeartely in 
+// fname and lname so the assigned number values are not need for this file.
+// fname = "Guvi";
+// lname = "geek"
+// admin = fname+lname;
+// alert( admin ); // "Guvi geek"
+
+// let fname=10.5; 
+// Here fname is not need hence the required alert is for only name
+// fname = "Guvi";
+// lname = "geek"
+// let name = fname+lname;
+// alert( 'hello ${name}' );
+
+// let a = prompt("Enter First number");
+// let b = prompt("Enter Second number");
+// The values have to be entered to get the popup alert.
+// alert(a + b);
+
+// var a = "2";
+// The code is blasted because the condition is verified in the variable 
+// declaration which makes the code blast.Assigning seperate variable for the 
+// value 12 will make the code diffused.
+// var b = "12";
+// //Don't touch below this
+// if (a) {
+//   console.log("Code is Blasted")
+// }
+// else
+// {
+//   console.log("Diffused") 
+// }
+
+// let a = prompt("Enter a number");
+// Entering the numbers will make the code run,other than that values will
+// make the code success. 
+// //Don't modify any code below this
+// if (a) {
+//  console.log( 'OMG it works for any number inc 0' );
+// }
+// else
+// {
+//  console.log( "Success" );
+// }
+
+// let value = prompt('How many runs you scored in this ball');
+// The run scored will be 4 and 6 if the value of input user is that,or
+// it will be couldn't figure out as there are condition for two scores only.
+// if (value === 4) {
+//       console.log("You hit a Four");
+// } else if (value === 6) {
+//       console.log("You hit a Six");
+// } else {
+//       console.log("I couldn't figure out");
+// }
+
+// let login = 'Employee';
+// The ternary operator has misplaced.
+// let message = (login == 'Employee') ? 
+//   (login == 'Director') : 'Greetings' :
+//   (login == '') ? 'No login' :
+//   '';
+// console.log(message);
+
+// let message;
+// if (message == undefined )
+// The variable condition is undefined so that is far enough to
+// check the condition.
+// {
+//  let message = "welcome boss";
+// }
+// else
+// {
+//  let message = "Go away";
+// }
+//   console.log(message); -->
+
+// let i = 3;
+// The condition is applied and decrement is made after the while loop.
+// while (i < 4) {
+//   console.log(i);
+// }i--;
+
+// for (let num = 2; num <= 20; num += 1) {
+//   if(num % 2==0){
+//   console.log(num)
+//   }
+// }
+
+// let gifts = ["teddy bear", "drone", "doll"];
+// for (let i = 0; i < 3; i++) {
+//   console.log(`Wrapped ${'gifts[i]'} and added a bow`);
+// } 
+
+// let countdown = 100;
+// while (countdown > 0) {
+//   countdown--;
+//   if(countdown !== 0)
+//   {
+//    console.log("bomb triggered");
+//   }
+// }
+
+// var lemein = “0”;
+// var lemeout = 0;
+// var msg = “”;
+// if (lemein) {
+//  msg += “hi”;
+//  }
+// if (lemeout) {
+//  msg += ‘Hello’;
+// }
+// console.log(msg);
+// The msg printed will be "hi" 'Hello' because both if
+// condition gets satisfied and console in output
+
